e013fb485996dfd51f8384598f8ac65b854178cd
pauldhalluin <paul.dhalluin@mines-paristech.fr>
2020-01-31 14:04:27 +0100

    des pitits changements

--- revelation.py
+++ revelation.py
@@ -6,20 +6,24 @@ def revelation(map, map_decouverte, coord, di, dj):
                 i = coord[0] + direction[0]
                 j = coord[1] + direction[1]
                 if map[i][j] == "#" or map[i][j] == "+":
-                    map_decouverte[i][j] == True
+                    map_decouverte[i][j] = True
     elif map[coord[0]][coord[1]] == "+":
-        indices = np.array(coord) + np.array([di, dj])
-        while map[indices[0] - 1][indices[1]] != "-":
-            indices[0] -= 1
-        while map[indices[0]][indices[1] - 1] != "¦":
-            indices[1] -= 1
-        i0, j0 = indices[0], indices[1]
-        while map[indices[0]][indices[1] + 1] != "¦":
-            indices[1] += 1
-        j1 = indices[1]
-        while map[indices[0] + 1][indices[1]] != "-":
-            indices[0] += 1
-        i1 = indices[0]
-        for i in range(i0-1, i1 + 2):
-            for j in range(j0-1, j1 + 2):
-                map_decouverte[i][j] == True
+        coord1 = coord - np.array(di, dj)
+        if map[coord1[0]][coord1[1]] == ".":
+            map_decouverte[coord[0] + di][coord[1] + dj] = True
+        else:
+            indices = np.array(coord) + np.array([di, dj])
+            while map[indices[0] - 1][indices[1]] != "-":
+                indices[0] -= 1
+            while map[indices[0]][indices[1] - 1] != "¦":
+                indices[1] -= 1
+            i0, j0 = indices[0], indices[1]
+            while map[indices[0]][indices[1] + 1] != "¦":
+                indices[1] += 1
+            j1 = indices[1]
+            while map[indices[0] + 1][indices[1]] != "-":
+                indices[0] += 1
+            i1 = indices[0]
+            for i in range(i0-1, i1 + 2):
+                for j in range(j0-1, j1 + 2):
+                    map_decouverte[i][j] = True
